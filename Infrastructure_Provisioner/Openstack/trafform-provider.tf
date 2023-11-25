f1c1312dadf97c50681f471043dcba2d6651a3ec
universcom <hamed.enayatzare@gmail.com>
2023-11-25 15:43:40 +0330

    Define Openstack Provider Trafform

---
+++ Infrastructure_Provisioner/Openstack/trafform-provider.tf
@@ -0,0 +1,20 @@
+# Define required providers
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.53.0"
+    }
+  }
+}
+
+# Configure the OpenStack Provider
+provider "openstack" {
+  user_name    = var.auth_username
+  tenant_name  = var.auth_tenant
+  password     = var.auth_password
+  auth_url     = var.auth_url
+  region       = var.auth_region
+  domain_name  = var.auth_domain
+}
---
+++ Infrastructure_Provisioner/Openstack/variables.tf
@@ -0,0 +1,57 @@
+# Authentication variables
+variable "auth_username" {}
+variable "auth_password" {}
+variable "auth_tenant" {}
+variable "auth_url" {}
+variable "auth_region" { default = "RegionOne" }
+variable "auth_domain" { default = "default" }
+
+
+#Kuberntes Specifications
+
+variable "OS_IMG_ID" {}
+variable "OS_flavor_ID" {}
+variable "OS_CIDR" {}
+
+variable "Number_Of_Masters" {
+  type = number
+  default = 1
+  validation {
+    condition = var.Number_Of_Masters % 2 == 1
+    error_message = "Number of masters must be odd like 1 or 3 or 5 or .."
+  }
+}
+
+variable "Number_of_Workers" {
+  type = number
+  default = 1
+  validation {
+    condition = var.Number_of_Workers >= 1
+    error_message = "You have to have at least 1 worker node"
+  }
+}
+
+variable "seprated_ETCD" {
+  type = bool
+  default = false
+}
+
+variable "network_type" {
+    type = string
+    default = "calico"
+  
+}
+
+variable "Ingress_type" {
+    type = string
+    default = "nginx"
+}
+
+variable "POD_CIDR" {}
+
+variable "Service_CIDR" {}
+
+variable "registry_mirror" {
+    type = string
+    default = ""
+}
