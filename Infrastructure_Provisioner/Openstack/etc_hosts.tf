d9dc2e4646ddc44279921238a9b8bdf7b2265533
universcom <hamed.enayatzare@gmail.com>
2023-12-02 08:34:10 +0330

    refine access to etc_hosts resources and variables

--- Infrastructure_Provisioner/Openstack/etc_hosts.tf
+++ Infrastructure_Provisioner/Openstack/etc_hosts.tf
@@ -1,14 +1,14 @@
-resource "local_file" "nginx" {
+resource "local_file" "etc_hosts" {
   depends_on = [ openstack_compute_floatingip_associate_v2.LB_floatingIP-associate ]
   filename = "config_files/hosts"
   content = <<-EOF
 127.0.0.1 localhosts
-%{ for index , instance in Master_nodes }
-${instance.access_ip_v4}  Master${index}.${cluster_FQDN} Master${index}
+%{ for index , instance in openstack_compute_instance_v2.kubernetes_Master_Instances }
+${instance.access_ip_v4}  Master${index}.${var.cluster_FQDN} Master${index}
 %{ endfor }
-%{ for index , instance in Worker_nodes }
-${instance.access_ip_v4}  Worker${index}.${cluster_FQDN} Worker${index}
+%{ for index , instance in openstack_compute_instance_v2.kubernetes_Worker_Instances }
+${instance.access_ip_v4}  Worker${index}.${var.cluster_FQDN} Worker${index}
 %{ endfor }
-${LB_node.access_ip_v4}  LB.${cluster_FQDN}  LB
+${openstack_compute_instance_v2.kubernetes_LB_Instance.access_ip_v4}  LB.${var.cluster_FQDN}  LB
   EOF
 }
