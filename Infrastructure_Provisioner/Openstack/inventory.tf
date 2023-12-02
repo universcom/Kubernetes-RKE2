5e2d2d1b6b74151d373b0601dadca9f9f68f765a
universcom <hamed.enayatzare@gmail.com>
2023-12-02 08:34:31 +0330

    refine access to inventory resources and variables

--- Infrastructure_Provisioner/Openstack/inventory.tf
+++ Infrastructure_Provisioner/Openstack/inventory.tf
@@ -4,13 +4,13 @@ resource "local_file" "anisble_inventory" {
   content = <<-EOF
 [all]
 localhost  ansible_host=127.0.0.1
-%{ for index , instance in Master_nodes }
-master${index}.${cluster_FQDN}  ansible_host=${instance.access_ip_v4}
+%{ for index , instance in openstack_compute_instance_v2.kubernetes_Master_Instances }
+master${index}.${var.cluster_FQDN}  ansible_host=${instance.access_ip_v4}
 %{ endfor}
-%{ for index , instance in Worker_nodes }
-worker${index}.${cluster_FQDN}  ansible_host=${instance.access_ip_v4}
+%{ for index , instance in openstack_compute_instance_v2.kubernetes_Worker_Instances }
+worker${index}.${var.cluster_FQDN}  ansible_host=${instance.access_ip_v4}
 %{ endfor}
-lb${index}.${cluster_FQDN}  ansible_host=${instance.access_ip_v4}
+lb.${var.cluster_FQDN}  ansible_host=${openstack_compute_instance_v2.kubernetes_LB_Instance.access_ip_v4}
 
 [all:vars]
 ansible_ssh_user=ubuntu
