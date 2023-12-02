b61aed7de9288a50959f58568be68fa28dfcd0a5
universcom <hamed.enayatzare@gmail.com>
2023-12-02 15:25:05 +0330

    init LB

---
+++ Infrastructure_Provisioner/Openstack/init_instances.tf
@@ -0,0 +1,28 @@
+resource "null_resource" "init_LB" {
+  depends_on = [
+  module.lunch_instances
+  ]
+  #connect to instances with ssh
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    timeout = "5m"
+    private_key = "${openstack_compute_keypair_v2.kubernetes_admin_user_key.private_key}"
+    host = "${openstack_compute_instance_v2.kubernetes_LB_Instance.floating_ip}"
+    port = "22"
+  }
+  provisioner "remote-exec" {
+    inline = [ 
+        "sudo apt update",
+        "sudo apt install nginx",
+        "sudo chown ubuntu:root -R /etc/nginx/*", 
+        "sudo chown ubuntu:root -R /etc/etc/hosts",
+        "sudo ssh-keygen && sudo echo ${openstack_compute_keypair_v2.kubernetes_admin_user_key.private_key} > /root/.ssh/id_rsa"
+     ]
+  }
+  #Copy required files to destination hosts
+  provisioner "file" {
+    source = "config_files/hosts"
+    destination = "/etc/hosts"
+  }
+}
